9a0643138eaaec23fb65d71b7741c865b42725d7
Moonika1991 <monika9107@gmail.com>
2020-10-07 08:45:52 +0200

    Calculator backend finished

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 import sys
+import math
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+
+
+class Button:
+    def __init__(self, text, result):
+        self.b = QPushButton(str(text))
+        self.text = text
+        self.result = result
+        self.b.clicked.connect(lambda: self.handleInput(self.text))
+
+
+    def handleInput(self, v):
+        if v == "=":
+            res = eval(self.result.text())
+            self.result.setText(str(res))
+        elif v == "AC":
+            self.result.setText("")
+        elif v == "√":
+            value = float(self.result.text())
+            self.result.setText(str(math.sqrt(value)))
+        elif v == "DEL":
+            current_value = self.result.text()
+            self.result.setText(current_value[:-1])
+        else:
+            current_value = self.result.text()
+            new_value = current_value + str(v)
+            self.result.setText(new_value)
 
 
 class Application(QWidget):
@@ -14,25 +41,31 @@ class Application(QWidget):
 
         #Create grid
         grid = QGridLayout()
+        result = QLineEdit()
 
-        buttons = ["AC", "C", "CE", "/",
+        buttons = ["AC", "DEL", "√", "/",
                    7, 8, 9, "*",
                    4, 5, 6, "-",
                    1, 2, 3, "+",
                    0, ".", "="]
 
-        row = 0
+        grid.addWidget(result, 0, 0, 1, 4)
+
+        row = 1
         col = 0
 
         for button in buttons:
             if col > 3:
                 col = 0
                 row += 1
+
+            buttonObject = Button(button, result)
+
             if button == 0:
-                grid.addWidget(QPushButton(str(button)), row, col, 1, 2)
+                grid.addWidget(buttonObject.b, row, col, 1, 2)
                 col += 1
             else:
-                grid.addWidget(QPushButton(str(button)), row, col, 1, 1)
+                grid.addWidget(buttonObject.b, row, col, 1, 1)
 
             col += 1
 
